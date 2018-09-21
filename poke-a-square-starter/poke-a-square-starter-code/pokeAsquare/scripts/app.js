e0a25b147acce17a2fda22b2e2a602033d50ffaa
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-09-21 11:41:17 -0500

    did the thing

--- poke-a-square-starter/poke-a-square-starter-code/pokeAsquare/scripts/app.js
+++ poke-a-square-starter/poke-a-square-starter-code/pokeAsquare/scripts/app.js
@@ -1,1 +1,21 @@
 
+$('button').on('click', () => {
+  console.log('click works');
+  //when the user clicks the button, populate the square
+  createSquare(30);
+});
+
+//Create a function named createSquare, that takes a
+//parameter of an arbitary number and creates
+//divs according to the number in the square class
+
+const createSquare = (numberOfSquares) => {
+
+  for (let i = 0; i <= numberOfSquares; i++){
+    //creating squares
+    const $div = $('<div/>');
+    //attach to the square class
+    $('.squares').append($div);
+
+  }
+}
